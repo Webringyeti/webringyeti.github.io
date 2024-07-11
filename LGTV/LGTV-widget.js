2127ebc7cc101a59f69d7a2f4e00e6fa99a2a056
Yetiuard <138736990+Yetiuard@users.noreply.github.com>
2024-07-11 18:37:49 +0300

    sdas

--- LGTV/LGTV-widget.js
+++ LGTV/LGTV-widget.js
@@ -1,27 +1,18 @@
-// === ONIONRING-WIDGET ===
-//this file contains the code which builds the widget shown on each page in the ring. ctrl+f 'EDIT THIS' if you're looking to change the actual html of the widget
-
-var tag = document.getElementById(ringID); //find the widget on the page
-
-thisSite = window.location.href; //get the url of the site we're currently on
+var tag = document.getElementById(ringID);
+thisSite = window.location.href;
 thisIndex = null;
-
-// go through the site list to see if this site is on it and find its position
 for (i = 0; i < sites.length; i++) {
-  if (thisSite.startsWith(sites[i])) { //we use startswith so this will match any subdirectory, users can put the widget on multiple pages
-    thisIndex = i;
-    break; //when we've found the site, we don't need to search any more, so stop the loop
+  if (thisSite.startsWith(sites[i])) {
+      thisIndex = i;
+    break;
   }
 }
-
 function randomSite() {
-  otherSites = sites.slice(); //create a copy of the sites list
-  otherSites.splice(thisIndex, 1); //remove the current site so we don't just land on it again
+  otherSites = sites.slice();
+  otherSites.splice(thisIndex, 1);
   randomIndex = Math.floor(Math.random() * otherSites.length);
   location.href = otherSites[randomIndex];
 }
-
-//if we didn't find the site in the list, the widget displays a warning instead
 if (thisIndex == null) {
   tag.insertAdjacentHTML('afterbegin', `
 <table>
@@ -32,25 +23,18 @@ if (thisIndex == null) {
   `);
 }
 else {
-  //find the 'next' and 'previous' sites in the ring. this code looks complex
-  //because it's using a shorthand version of an if-else statement to make sure
-  //the first and last sites in the ring join together correctly
   previousIndex = (thisIndex-1 < 0) ? sites.length-1 : thisIndex-1;
   nextIndex = (thisIndex+1 >= sites.length) ? 0 : thisIndex+1;
 
   indexText = ""
-  //if you've chosen to include an index, this builds the link to that
   if (useIndex) {
-    indexText = `<a href='${indexPage}'>index</a> | `;
+    indexText = `<a href='${indexPage}'>index</a>  `;
   }
 
   randomText = ""
-  //if you've chosen to include a random button, this builds the link that does that
   if (useRandom) {
-    randomText = `<a href='javascript:void(0)' onclick='randomSite()'>random</a> | `;
+    randomText = `<a href='javascript:void(0)' onclick='randomSite()'>random</a>  `;
   }
-
-  //this is the code that displays the widget - EDIT THIS if you want to change the structure
   tag.insertAdjacentHTML('afterbegin', `
   <p>Part of the <a href="https://webring.yetiuard.wtf">LGTV Webring</a>!</p>
   <a href="${sites[previousIndex]}">Previous</a>
