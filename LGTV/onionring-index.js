91f3a1abd5f51246145c4a7c828dbefc2de0e3d4
Yetiuard <138736990+Yetiuard@users.noreply.github.com>
2024-07-11 14:11:50 +0300

    s

--- LGTV/onionring-index.js
+++ LGTV/onionring-index.js
@@ -1,9 +1,14 @@
+// === ONIONRING-INDEX ===
+//this file builds the list of sites in the ring for displaying on your index page
+
 var tag = document.getElementById('index');
 regex = /^https:\/\/|\/$/g;
+
 list = "";
 for (i = 0; i < sites.length; i++) {
   list += `<li><a href='${sites[i]}'>${sites[i].replace(regex, "")}</a></li>`;
 }
+
 tag.insertAdjacentHTML('afterbegin', `
 <ul>
 ${list}
